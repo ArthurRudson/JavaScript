93599f2f11d69b667df1cd4c0a913f4b292c7282
ArthurRudson <154820615+ArthurRudson@users.noreply.github.com>
2024-04-11 17:40:15 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 66
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if (vel > 60) { //Condição simple!
+    console.log(`Voce ultrapassou a velociade premitida. MULTADO!`)
+}
+console.log('Dirija sempre usando o sinto de segurança!')
