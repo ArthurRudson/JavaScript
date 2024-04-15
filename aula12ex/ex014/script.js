32d0a16bd7015c1ac8f54441a88ebf5389ea7c4a
ArthurRudson <154820615+ArthurRudson@users.noreply.github.com>
2024-04-15 17:10:15 -0300

    ex14

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,14 +1,14 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('foto')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12 ) {
-        img.src = 'manha.jpg'
+        img.src = 'imagem/manha.jpg'
     } else if (hora >= 12 && hora < 18) {
-        img.src = 'tarde.jpg'
+        img.src = 'imagem/tarde.jpg'
     } else {
-        img.src = 'noite.jpg'
+        img.src = 'imagem/noite.jpg'
     }
 }
